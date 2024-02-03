94dee9307842c754e89f54ad46e2658341412828
Ivan Frolov <ivan-anatoliu@ya.ru>
2024-02-03 20:14:37 +0300

    second task "Большая задача по Maps API. Часть №5"

--- main_window.py
+++ main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QTextEdit
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 import requests
@@ -8,7 +8,14 @@ from io import BytesIO
 
 class Map(QMainWindow):
     def __init__(self):
+        self.search = None
+        self.textedit = None
+        self.switch = None
+        self.pic = None
+        self.pic_label = None
         self.API = 'https://static-maps.yandex.ru/1.x/'
+        self.API_GEOCODER = 'http://geocode-maps.yandex.ru/1.x/'
+        self.API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
         self.kind = 'hybrid'
         try:
             self.longitude = float(input('Введите долготу >>> '))
@@ -18,16 +25,46 @@ class Map(QMainWindow):
             print('Неверный ввод. Должно быть числом с плавающей точкой')
             quit()
         super().__init__()
-        self.pic = None
-        self.pic_label = None
         self.setWindowTitle('Map preview')
         self.setGeometry(100, 100, 1000, 500)
+        self.creator()
         self.show_pic()
+        self.switcher()
+
+    def creator(self):
         self.switch = QPushButton(self)
         self.switch.setGeometry(10, 10, 150, 30)
         self.switch.clicked.connect(self.switcher)
         self.switch.show()
-        self.switcher()
+
+        self.textedit = QTextEdit(self)
+        self.textedit.setGeometry(170, 10, 150, 30)
+        self.textedit.show()
+
+        self.search = QPushButton(self)
+        self.search.setGeometry(330, 10, 150, 30)
+        self.search.clicked.connect(self.searcher)
+        self.search.setText('Искать')
+        self.search.show()
+
+    def searcher(self):
+        if not self.textedit.toPlainText():
+            return None
+        print(self.textedit.toPlainText())
+        params = {
+            "apikey": self.API_KEY,
+            "geocode": self.textedit.toPlainText(),
+            "format": 'json'
+        }
+
+        response = requests.get(self.API_GEOCODER, params)
+        if response.status_code != 200:
+            print(response.reason)
+            quit()
+        self.longitude, self.latitude = map(lambda x: float(x), response.json()["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]["Point"]["pos"].split(" "))
+        self.length = 0.1
+        self.new_map()
 
     def show_pic(self, name='map'):
         pic = QPixmap(f"{name}.png")
